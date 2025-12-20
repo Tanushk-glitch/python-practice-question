343430a9550b4205628496dbfab88422390efc3e
Tanushk Baranwal <baranwaltanushk@gmail.com>
2025-12-20 14:16:32 +0530

    Add files via upload

---
+++ python practice/Q8.py
@@ -0,0 +1,12 @@
+#Count Vowels Using Loop & Condition
+
+text = "python"
+vowels = "aeiou"
+count = 0
+
+for ch in text:
+    if ch in vowels:
+        count += 1
+
+print(count)
+# The code counts the number of vowels in the string "python" using a loop and condition.
